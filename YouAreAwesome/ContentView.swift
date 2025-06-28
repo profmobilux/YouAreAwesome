8ba330fe3007b3d2256af0ea24cd9f36eb1df65e
profmobilux <services@philipphermann.com>
2025-06-28 20:31:50 +0200

    buttons, text changes, frame

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,20 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var myText = "You are awesome!"
     var body: some View {
         
         
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You Are Awesome!")
-                .fontWeight(.heavy)
+                .frame(width: 200, height: 200)
+             
+            Text(myText)
+                .fontWeight(.ultraLight)
                 .font(.largeTitle)
-                .foregroundStyle(.red)
+                .foregroundStyle(.black)
+            
+            Spacer()
+            
+            HStack {
+                Button("Click Me") {
+                    print("you clicked me")
+                    myText = "You are the best!"
+                }
+
                 
-                
+                Button("Click Me") {
+                    print("you clicked me")
+                    myText = "No, I swear!"
+                }
+
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(Color.red)
+        
+            
         }
         .padding()
     }
